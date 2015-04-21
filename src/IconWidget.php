9838e9ab27d14844eff7f6d81179256f72fd828e
David Molineus <david.molineus@netzmacht.de>
2015-04-21 07:12:14 +0200

    Reindent.

--- src/IconWidget.php
+++ src/IconWidget.php
@@ -18,169 +18,169 @@ namespace Netzmacht\Contao\IconWizard;
  */
 class IconWidget extends \TextField
 {
-	/**
-	 * Template
-	 * @var string
-	 */
-	protected $strTemplate = 'be_widget';
+    /**
+     * Template
+     * @var string
+     */
+    protected $strTemplate = 'be_widget';
 
-	/**
-	 * @var
-	 */
-	protected $arrIcons;
+    /**
+     * @var
+     */
+    protected $arrIcons;
 
-	/**
-	 * @var
-	 */
-	protected $strIconTemplate;
+    /**
+     * @var
+     */
+    protected $strIconTemplate;
 
 
-	/**
-	 * set default iconTemplate
+    /**
+     * set default iconTemplate
      *
-	 * @param null $arrAttributes
+     * @param null $arrAttributes
      * @SuppressWarnings(PHPMD.Superglobals)
-	 */
-	public function __construct($arrAttributes=null)
-	{
-		$this->iconTemplate = $GLOBALS['TL_CONFIG']['iconWizardIconTemplate'];
-		parent::__construct($arrAttributes);
-	}
+     */
+    public function __construct($arrAttributes=null)
+    {
+        $this->iconTemplate = $GLOBALS['TL_CONFIG']['iconWizardIconTemplate'];
+        parent::__construct($arrAttributes);
+    }
 
 
-	/**
-	 * @param string $key
-	 * @return mixed|string
-	 */
-	public function __get($key)
-	{
-		switch($key) {
-			case 'icon':
-				return $this->varValue;
-				break;
+    /**
+     * @param string $key
+     * @return mixed|string
+     */
+    public function __get($key)
+    {
+        switch($key) {
+            case 'icon':
+                return $this->varValue;
+                break;
 
-			case 'icons':
-			case 'options':
-				return $this->arrIcons;
-				break;
+            case 'icons':
+            case 'options':
+                return $this->arrIcons;
+                break;
 
-			case 'iconTemplate':
-				return $this->strIconTemplate;
-				break;
+            case 'iconTemplate':
+                return $this->strIconTemplate;
+                break;
 
-			default:
-				return parent::__get($key);
-				break;
-		}
-	}
-
-
-	/**
-	 * @param string $key
-	 * @param mixed $value
-	 */
-	public function __set($key, $value)
-	{
-		switch($key) {
-			case 'icon':
-				$this->varValue = $value;
-				break;
-
-			case 'icons':
-			case 'options':
-				$this->arrIcons = $value;
-				break;
-
-			case 'iconTemplate':
-				$this->strIconTemplate = $value;
-				break;
-
-			default:
-				parent::__set($key, $value);
-				break;
-		}
-	}
+            default:
+                return parent::__get($key);
+                break;
+        }
+    }
 
 
-	/**
-	 * @param mixed $value
-	 * @return mixed
+    /**
+     * @param string $key
+     * @param mixed $value
+     */
+    public function __set($key, $value)
+    {
+        switch($key) {
+            case 'icon':
+                $this->varValue = $value;
+                break;
+
+            case 'icons':
+            case 'options':
+                $this->arrIcons = $value;
+                break;
+
+            case 'iconTemplate':
+                $this->strIconTemplate = $value;
+                break;
+
+            default:
+                parent::__set($key, $value);
+                break;
+        }
+    }
+
+
+    /**
+     * @param mixed $value
+     * @return mixed
      * @SuppressWarnings(PHPMD.Superglobals)
-	 */
-	protected function validator($value)
-	{
-		$value = parent::validator($value);
+     */
+    protected function validator($value)
+    {
+        $value = parent::validator($value);
 
-		if($this->hasErrors()) {
-			return;
-		}
-		elseif($value == '')
-		{
-			if($this->mandatory) {
-				$this->addError($this->addError(sprintf($GLOBALS['TL_LANG']['ERR']['mandatory'], $this->strLabel)));
-			}
-		}
-		elseif(!$this->iconExists($value)) {
-			$this->addError(sprintf($GLOBALS['TL_LANG']['ERR']['iconNotFound'], $this->strLabel));
-		}
+        if($this->hasErrors()) {
+            return;
+        }
+        elseif($value == '')
+        {
+            if($this->mandatory) {
+                $this->addError($this->addError(sprintf($GLOBALS['TL_LANG']['ERR']['mandatory'], $this->strLabel)));
+            }
+        }
+        elseif(!$this->iconExists($value)) {
+            $this->addError(sprintf($GLOBALS['TL_LANG']['ERR']['iconNotFound'], $this->strLabel));
+        }
 
-		return $value;
-	}
+        return $value;
+    }
 
 
-	/**
-	 * @return string
+    /**
+     * @return string
      * @SuppressWarnings(PHPMD.Superglobals)
-	 */
-	public function generate()
-	{
-		$url = sprintf('system/modules/icon-wizard/public/popup.php?table=%s&amp;field=%s&amp;name=ctrl_%s&amp;id=%s',
-			\Input::get('table'), $this->strField, $this->name, \Input::get('id')
-		);
+     */
+    public function generate()
+    {
+        $url = sprintf('system/modules/icon-wizard/public/popup.php?table=%s&amp;field=%s&amp;name=ctrl_%s&amp;id=%s',
+            \Input::get('table'), $this->strField, $this->name, \Input::get('id')
+        );
 
-		return sprintf(
-			'<div class="iconWizard"><input type="hidden" name="%s" id="ctrl_%s" value="%s"%s'
-			. '<span class="icon">%s</span> <span class="title">%s</span>'
-			. ' <a href="%s" onclick="Backend.getScrollOffset();Backend.openModalIframe({url:\'%s\', width: %s, title: \'%s\'});return false"'
-			. ' title="%s" class="tl_submit">%s</a></div>',
-			$this->name,
-			$this->name,
-			$this->icon,
-			$this->strTagEnding,
-			$this->icon ? sprintf($this->iconTemplate, $this->icon) : '',
-			$this->icon ? $this->icon : '-',
-			$url,
-			$url,
-			790,
-			$GLOBALS['TL_LANG']['MSC']['iconWizard'][0],
-			$GLOBALS['TL_LANG']['MSC']['iconWizardlink'][1],
-			$GLOBALS['TL_LANG']['MSC']['iconWizardlink'][0]
-		);
-	}
+        return sprintf(
+            '<div class="iconWizard"><input type="hidden" name="%s" id="ctrl_%s" value="%s"%s'
+            . '<span class="icon">%s</span> <span class="title">%s</span>'
+            . ' <a href="%s" onclick="Backend.getScrollOffset();Backend.openModalIframe({url:\'%s\', width: %s, title: \'%s\'});return false"'
+            . ' title="%s" class="tl_submit">%s</a></div>',
+            $this->name,
+            $this->name,
+            $this->icon,
+            $this->strTagEnding,
+            $this->icon ? sprintf($this->iconTemplate, $this->icon) : '',
+            $this->icon ? $this->icon : '-',
+            $url,
+            $url,
+            790,
+            $GLOBALS['TL_LANG']['MSC']['iconWizard'][0],
+            $GLOBALS['TL_LANG']['MSC']['iconWizardlink'][1],
+            $GLOBALS['TL_LANG']['MSC']['iconWizardlink'][0]
+        );
+    }
 
 
-	/**
-	 * check if icon exists
-	 *
-	 * @param $icon
-	 * @return bool
-	 *
-	 */
-	protected function iconExists($icon)
-	{
-		foreach($this->icons as $n => $group)
-		{
-			foreach($group as $entry) {
-				if(!is_array($entry)) {
-					continue;
-				}
+    /**
+     * check if icon exists
+     *
+     * @param $icon
+     * @return bool
+     *
+     */
+    protected function iconExists($icon)
+    {
+        foreach($this->icons as $group)
+        {
+            foreach($group as $entry) {
+                if(!is_array($entry)) {
+                    continue;
+                }
 
-				if($entry['value'] == $icon) {
-					return true;
-				}
-			}
-		}
+                if($entry['value'] == $icon) {
+                    return true;
+                }
+            }
+        }
 
-		return false;
-	}
+        return false;
+    }
 }
