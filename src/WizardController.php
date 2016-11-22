d86f6ef388615315b43530bb74293e56252399e0
David Molineus <david.molineus@netzmacht.de>
2016-11-22 16:23:38 +0100

    Add missing author.

--- src/WizardController.php
+++ src/WizardController.php
@@ -3,6 +3,7 @@
 /**
  * @package    icon-wizard
  * @author     David Molineus <david.molineus@netzmacht.de>
+ * @author     Marcel <ms@rtft.de>
  * @copyright  2013-2015 netzmacht creative David Molineus
  * @license    LGPL 3.0
  * @filesource
